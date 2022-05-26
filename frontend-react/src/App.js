0b285f9637ff80fbb36eadde9364e2ebf57c498f
AngerOverApathy <angersavestheworld@gmail.com>
2022-05-25 21:57:35 -0700

    react donate page created/lightly styled

---
+++ frontend-react/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Stripe from './components/Stripe';
+import fiveDollar from './assets/5dollar.webp'
+import tenDollar from './assets/ten.jpg'
+import { useState } from 'react';
+
+function App() {
+  const [showItem, setShowItem] = useState(false)
+  return (
+    <div className="App">
+      <h1>Donate</h1>
+        <div className='donateOptions'>
+          {showItem ? <Stripe/> : <> <h3>$5.00</h3> <img id='five' src={fiveDollar} 
+          alt='five dollar donation'/>
+          <button onClick={()=> setShowItem(true)}>Donate $5</button></>}
+          {/* {showItem ? <Stripe/> : <> <h3>$10.00</h3> <img id='ten' src={tenDollar} 
+          alt='ten dollar donation'/>
+          <button onClick={()=> setShowItem(true)}>Donate $10</button></>} */}
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend-react/src/components/PayForm.js
@@ -0,0 +1,75 @@
+import React, {useState} from 'react'
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js'
+import axios from 'axios' //call to backend
+
+const CARD_OPTIONS = {
+	iconStyle: "solid",
+	style: {
+		base: {
+			iconColor: "#c4f0ff",
+			color: "#fff",
+			fontWeight: 500,
+			fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
+			fontSize: "16px",
+			fontSmoothing: "antialiased",
+			":-webkit-autofill": { color: "#fce883" },
+			"::placeholder": { color: "#87bbfd" }
+		},
+		invalid: {
+			iconColor: "#ffc7ee",
+			color: "#ffc7ee"
+		}
+	}
+}
+
+export default function PayForm() {
+    const [success, setSuccess] = useState(false)
+    const stripe = useStripe()
+    const elements = useElements()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const {error, paymentMethod} = await stripe.createPaymentMethod({
+            type: 'card',
+            card: elements.getElement(CardElement)
+        })
+
+        if(!error) {
+            try {
+                const {id} = paymentMethod
+                const response = await axios.post('http://localhost:3000/donate', {
+                    amount: 500, //$5 in cents
+                    id: id
+                })
+                if(response.data.success) {
+                    console.log('Thanks for donating')
+                    setSuccess(true)
+                }
+            } catch (error) {
+                console.log('Error:', error)
+            }
+        } else {
+            console.log(error)
+        }
+    }
+
+
+  return (
+    <>
+      {!success ?
+      <form onSubmit={handleSubmit}>
+          <fieldset className='FormGroup'>
+              <div className='FormRow'>
+                  <CardElement options={CARD_OPTIONS} />
+              </div>
+          </fieldset>
+          <button>Donate</button>
+      </form>
+      :
+      <div>
+          <h2>Thanks for donating!</h2>
+      </div>
+      }
+    </>
+  )
+}
