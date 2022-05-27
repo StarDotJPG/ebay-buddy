4e7e26aeba7c8af5cce4754bba9cc0712fa1375b
Aaron Weaver <95551676+Atweaver7@users.noreply.github.com>
2022-05-26 20:14:40 -0700

    made search prettier

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Header />
       <main>
       <Search />
-      <Item/>
+      {/* <Item/> */}
       </main>
     </div>
   );
--- Develop/client/src/components/Search.jsx
+++ Develop/client/src/components/Search.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import "../assets/styles/Search.css";
-
-
-
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 
 
@@ -10,24 +9,30 @@ import "../assets/styles/Search.css";
 function Search() {
   function itemSearch(event) {
     event.preventDefault();
-    let item = document.getElementById("itemName").value
-    console.log(item)
-}
+    let item = document.getElementById("itemName").value;
+    console.log(item);
+  }
   return (
     <main class="background">
-        <form onSubmit={(event) => {itemSearch(event)}}>
+      <form
+        onSubmit={(event) => {
+          itemSearch(event);
+        }}
+      >
         <div class="text-center container">
-            <label>Item Name:  </label>
-            <input class="" id="itemName" type="text" name="itemName"></input>
-            <button class ="">GO TIME BABY!!!</button>
+          <label>Item Name: </label>
+          <TextField class="" id="itemName" label="itemName" variant="outlined" />
+         
+          <Button variant="contained">GO TIME BABY!!!</Button>
+         
         </div>
         <div class="drop-down text-center container">
-        <select class="" name="sections" id="sections">
-          <option class="" value="A1"></option>
-        </select>
+          <select class="" name="sections" id="sections">
+            <option class="" value="A1"></option>
+          </select>
         </div>
-
-        </form>
+      </form>
+    
     </main>
   );
 }
