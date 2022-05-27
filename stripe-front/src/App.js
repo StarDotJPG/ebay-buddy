8a43b49a780a950cfe1f3ae294e79acaf91d9fa8
AngerOverApathy <angersavestheworld@gmail.com>
2022-05-26 17:22:30 -0700

    payments working

--- stripe-front/src/App.js
+++ stripe-front/src/App.js
@@ -6,24 +6,44 @@ function App() {
 
   const [donation, setDonation] = useState({
     name: 'Donation: $5',
-    price: 500
+    price: 5
   })
+
+  const makeDonation = token => {
+    const body = {
+      token,
+      donation
+    }
+    const headers = {
+      'Content-Type': 'application/json'
+    }
+    return fetch(`http://localhost:3001/donate`, {  //stripe will only work on httpS
+      method: 'POST',
+      headers,
+      body: JSON.stringify(body)
+    }).then(response => {
+      console.log('RESPONSE', response)
+      const {status} = response;
+      console.log('STATUS', status)
+    }).catch(error => console.log(error))
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
+        <StripeCheckout stripeKey='pk_test_51L3NlJIbWCRjsLJPg9Oew0LGLMS3vhlEQjb3Gs20yY0UCzvBLBdLSQqdFHKpdT2zXQsMO59uZrP1Pr4pmVF9OXiZ00wiD8sf8t' 
+                        token={makeDonation} 
+                        name='Donate'
+                        amount={donation.price * 100}>
+          <button className='donateBtn'>DONATE {donation.price}</button>
+        </StripeCheckout>
       </header>
     </div>
   );
