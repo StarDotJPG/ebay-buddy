8cde75604be21afad2b8c2e7a01bce10ed195728
AngerOverApathy <angersavestheworld@gmail.com>
2022-06-01 21:41:02 -0700

    react styling updated

--- Develop/client/src/components/Login/index.js
+++ Develop/client/src/components/Login/index.js
@@ -4,35 +4,39 @@ import './style.css'
 export default function Login() {
   return (
     <>
-<header class="loginHeader">
-    <h1>
-    <span style={{color: "#E53238"}}>e</span>
-    <span style={{color: "#0064D3"}}>b</span>
-    <span style={{color: "#F5A5"}}>a</span>
-    <span style={{color: "#86B817"}}>y</span>
-    <span style={{color: Blue}}>Buddy</span>
-    </h1>
-</header>
-    <div className='login'>
-          <h2>Login</h2>
-          <label>Username:</label>
-          <input type="text" name="Username" required />
-                <label>Password:</label>
-                <input type="password" name="password" required />
-    </div>
-        <div className="submitBtn">
-            <input type="submit" />
+    <header className="loginHeader">
+        <h1>
+        <span style={{color: "#E53238"}}>e</span>
+        <span style={{color: "#0064D3"}}>b</span>
+        <span style={{color: "#F5A5"}}>a</span>
+        <span style={{color: "#86B817"}}>y</span>
+        <span style={{color: "#0064D3"}}>Buddy</span>
+        </h1>
+    </header>
+        <div className="mainContainer">
+            <div className='login'>
+                <h2>Login</h2>
+                    <label>Email:</label>
+                        <input type="text" name="Email" required />
+                <br />
+                    <label>Password:</label>
+                        <input type="password" name="password" required />
+                            <div className="submitBtn">
+                                <input type="submit" />
+                            </div>
+            </div>
+        <div className='signup'>
+            <h2>Sign Up</h2>
+                <label>Email:</label>
+                    <input type="text" name="Email" required />
+                <br />
+                    <label>Password:</label>
+                        <input type="password" name="password" required />
+                            <div className="submitBtn">
+                                <input type="submit" />
+                            </div>
+            </div>
         </div>
-    <h2>Sign Up</h2>
-    <div className ='signup'>
-        <label>Username:</label>
-        <input type="text" name="Username" required />
-                <label>Password:</label>
-                <input type="password" name="password" required />
-    </div>
-    <div className="submitBtn">
-        <input type="submit" />
-    </div>
     </>
   )
 }
