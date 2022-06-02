941e905e06a5c9268eb05d8ed29aea772cad62df
Jason Greenia <jason.greenia@gmail.com>
2022-06-01 20:55:19 -0700

    made progress on getting graphQL response

--- Develop/client/src/components/Search/index.js
+++ Develop/client/src/components/Search/index.js
@@ -1,12 +1,38 @@
 import React from "react";
 import "./style.css";
 import TextField from "@mui/material/TextField";
+import { useQuery, userParam } from '@apollo/client';
+import { QUERY_ITEMS } from '../../utils/queries';
 
+const useSearchData = () => {
+  const { loading, error, data } = useQuery(QUERY_ITEMS)
+
+  //{ data } = data[]
+
+  if (loading) {
+    console.log("Loading...")
+  }
+
+  if (error) {
+    console.log(error.message)
+  }
+
+  console.log(data.stringify)
+
+  return data
+
+}
 
 function Search() {
+  
+  const itemsArray = useSearchData()
+
   function itemSearch(event) {
     event.preventDefault();
     let item = document.getElementById("itemName").value;
+    //Compare to array returned from GQL query
+    //data.foreach if data.name = item
+    //create list element with that name
     console.log(item);
   }
   return (
