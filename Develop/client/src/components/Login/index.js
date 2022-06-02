ec7422d4f3749b579c02bef098a4c4dd1bf7dbbe
KatyKedi <katykat5bros@gmail.com>
2022-06-01 21:11:38 -0700

    initial login page

---
+++ Develop/client/src/components/Login/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './style.css'
+
+export default function Login() {
+  return (
+    <>
+     <header class="loginHeader">
+    <h1>
+    <span style={{color: red}}>e</span>
+    <span style={{color: blue}}>b</span>
+    <span style={{color: yellow}}>a</span>
+    <span style={{color: "#86B817"}}>y</span>
+    <span style={{color: Blue}}>Buddy</span>
+    </h1>
+</header>
+    <div className='login'>
+          <h2>Login</h2>
+          <label>Username:</label>
+          <input type="text" name="Username" required />
+                <label>Password:</label>
+                <input type="password" name="password" required />
+    </div>
+        <div className="submitBtn">
+            <input type="submit" />
+        </div>
+    <h2>Sign Up</h2>
+    <div className ='signup'>
+        <label>Username:</label>
+        <input type="text" name="Username" required />
+                <label>Password:</label>
+                <input type="password" name="password" required />
+    </div>
+    <div className="submitBtn">
+        <input type="submit" />
+    </div>
+    </>
+  )
+}
